a452233e6c268017b912dfe9eeca821d6eb303e7
Julia Pe <pe_july@mail.ru>
2021-10-06 12:53:09 -0400

    drag-drop

---
+++ drag_drop/app.js
@@ -0,0 +1,48 @@
+const item = document.querySelector('.item')
+const placeholders = document.querySelectorAll('.placeholder')
+
+item.addEventListener('dragstart', dragstart)
+item.addEventListener('dragend', dragend)
+
+for (const placeholder of placeholders) {
+    console.log(placeholder)
+    placeholder.addEventListener('dragover', dragover)
+    placeholder.addEventListener('dragenter', dragenter)
+    placeholder.addEventListener('dragleave', dragleave)
+    placeholder.addEventListener('drop', dragdrop)
+
+}
+
+function dragstart (event) {
+    // console.log('drag start', event.target)
+    event.target.classList.add('hold')
+    setTimeout(() => event.target.classList.add ('hide'), 0)
+    //classList - объект у кот есть методы для манип классами
+}
+
+function dragend(event) {
+    event.target.className = 'item'
+    // event.target.classList.remove('hold', 'hide') can be used instead
+    //className всегда работает со строчками
+}
+
+function dragover(event) {
+    event.preventDefault()
+}
+
+function dragenter(event) {
+    event.target.classList.add('hovered')
+}
+
+function dragleave(event) {
+    event.target.classList.remove('hovered')
+}
+
+function dragdrop(event) {
+    event.target.classList.remove('hovered')
+    event.target.append(item)
+   
+}
+
+
+
